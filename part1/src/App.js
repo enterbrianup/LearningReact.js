5b946522df7bf2fba116b64a948ba8645a4532b3
enterbrianup <anup.aky52@gmail.com>
2023-08-14 17:15:26 +0530

    changes made

--- part1/src/App.js
+++ part1/src/App.js
@@ -678,34 +678,92 @@ import './App.css';
 //  Under     Passing state - to child components
 // Sharing state between components
 
-const Panel =({title, children})=>{
-  const [isActive, setisActive]=useState(false);
-  return(
-   <section className="Panel">
-   <h3>{title}</h3>
-   {isActive ? (
-    <p>{children}</p>
-   ) : (
-    <button onClick={() => setisActive(true)}>Show</button>
-   )}
-   </section>
-  );
-}
-  const App =()=>{
-  return (
-  <>
-  <h2> 
-      Almaty, Kazakhstan
-  </h2>
-  <Panel title="About">
-      With a population of about 2 million, Almaty is Kazakhstan's largest city. From 1929 to 1997, it was its capital city.
-  </Panel>
-  <Panel title="Ethymology">
-      The name comes from <span lang="kk-KZ">алма</span>, the Kazakh word for "apple" and is often translated as "full of apples". In fact, the region surrounding Almaty is thought to be the ancestral home of the apple, and the wild <i lang="la">Malus sieversii</i> is considered a likely candidate for the ancestor of the modern domestic apple.
+// const Panel =({title, children, isActive, onShow})=>{
   
-  </Panel>
-  </>)
+//   return(
+//    <section className="Panel">
+//    <h3>{title}</h3>
+//    {isActive ? (
+//     <p>{children}</p>
+//    ) : (
+//     <button onClick={onShow}>Show</button>
+//    )}
+//    </section>
+//   );
+// }
+//   const App =()=>{
+//     const [activeIndex, setActiveIndex] = useState(0);
+//   return (
+//   <>
+//   <h2> 
+//       Almaty, Kazakhstan
+//   </h2>
+//   <Panel title="About" isActive={activeIndex === 0}
+//   onShow={()=>setActiveIndex(0)}>
+//       With a population of about 2 million, Almaty is Kazakhstan's largest city. From 1929 to 1997, it was its capital city.
+//   </Panel>
+//       <Panel title="Ethymology" isActive={activeIndex === 1}
+//         onShow={() => setActiveIndex(1)}>
+//       The name comes from <span lang="kk-KZ">алма</span>, the Kazakh word for "apple" and is often translated as "full of apples". In fact, the region surrounding Almaty is thought to be the ancestral home of the apple, and the wild <i lang="la">Malus sieversii</i> is considered a likely candidate for the ancestor of the modern domestic apple.
+  
+//   </Panel>
+//   </>)
+// }
+
+
+//   Challenges Synced input
+
+// lifting state up
+// Remove state from the child components.
+// Pass hardcoded data from the common parent.
+// Add state to the common parent and pass it down together with the event handlers.
+
+const SyncedInput = ({ label, value, onChange })=>{ 
+
+  return (
+    <>
+    
+    <label>
+      {label}
+      {'  '}
+        <input 
+        value={value} 
+        onChange={onChange}
+      />
+      </label>
+      </>
+  );
+
+
+
 }
+const App = ()=>{
+
+  const [Text, setText] =useState('');
+  
+  const handleChange =(e)=>{
+    setText(e.target.value)
+  }
+  return (
+    <div>
+
+     <h1>lifting the state up</h1>
+       <SyncedInput 
+       label="First input"
+         value={Text}
+         onChange={handleChange}
+       />
+
+      <SyncedInput
+        label="second input"
+        value={Text}
+        onChange={handleChange}
+         />
+    </div>
+  );
+     
+}
+
 
 export default App;
 
